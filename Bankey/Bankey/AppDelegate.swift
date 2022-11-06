3fec4dd6caae109b0f2fc0392bce56f4bc675e2c
Larissa Pinheiro <lpinheiro@ciandt.com>
2022-11-06 17:21:54 -0300

    - created new project - removed storyboard and make it programmatically - created first view

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Larissa Virgínia dos Santos Pinheiro on 06/11/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+
+}
+
